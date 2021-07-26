ea37378e4cd1d3009850e7d929c8f4b51aac7385
aradhya7420 <aradhyasharma750@gmail.com>
2021-07-26 15:15:29 +0530

    Fix nav not response for smaller screens

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('<str:auction_id>/auctiondetail/', views.auctiondetail, name='auctiondetail'),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {}
+    title = "Home"
+    context = {'title':title}
     return render(request, 'auctions/home.html', context)
+
+def shop(request):
+    title = "All Auctions"
+    context = {'title':title}
+    return render(request, 'auctions/shop.html', context)
+
+def auctiondetail(request, auction_id):
+    title = "Details"
+    context = {'title':title}
+    return render(request, 'auctions/auctiondetail.html', context)
