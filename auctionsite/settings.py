ed1cdea1a02187c3d12d147db02a9b3c621bafe6
aradhya7420 <aradhyasharma750@gmail.com>
2021-08-10 13:52:18 +0530

    Attempt fix image not loading issue heroku

--- auctionsite/settings.py
+++ auctionsite/settings.py
@@ -127,11 +127,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'auctions/static/auctions/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media files upload directory
+MEDIA_URL = '/media/' # media files retrive directory
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- auctionsite/urls.py
+++ auctionsite/urls.py
@@ -17,11 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auctions.urls')),
-    path('currencies/', include('currencies.urls'))
+    path('currencies/', include('currencies.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
