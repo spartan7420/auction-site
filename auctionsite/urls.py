c506a7a4c331fa84b55c785dbff65e1bc70c2fa5
aradhya7420 <aradhyasharma750@gmail.com>
2021-08-10 16:35:36 +0530

    Fix image url issue

--- auctionsite/urls.py
+++ auctionsite/urls.py
@@ -25,8 +25,8 @@ urlpatterns = [
     path('', include('auctions.urls')),
     path('currencies/', include('currencies.urls')),
 
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
