ff3a0a49c9aa569aa904219a982ae10dc93e9b98
Aradhya Sharma <55148309+arads7420@users.noreply.github.com>
2023-01-12 16:08:43 +0530

    Add render.yaml file

--- auctionsite/settings.py
+++ auctionsite/settings.py
@@ -22,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+di)xr=gjqb$v^m)e)dm%lqmxn@o7#697e0_l(%4x%=&45v_kw'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-+di)xr=gjqb$v^m)e)dm%lqmxn@o7#697e0_l(%4x%=&45v_kw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
+
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
